4b3f8fc2867aa2dc95a3c9cc0af94d23bec782ae
tanvir <mstanvirislam05@gmail.com>
2026-08-08 20:44:39 +0600

    feat: add bcrypt and jsonwebtoken dependencies, update prisma schema, and refactor server startup
    - Added bcrypt and jsonwebtoken to dependencies in package.json
    - Updated devDependencies to include types for bcrypt and jsonwebtoken
    - Modified prisma schema to include USER model
    - Refactored server startup in server.ts to use an async function
    - Updated tsconfig.json to include src directory in compilation

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,7 @@ app.use(express.json());
 
 const port = process.env.PORT || 5000
 
-app.listen(port, () => {
-    console.log(`server is running in ${port}`);
-
-})
+const startServer = async () => {
+    app.listen(port)
+}
+startServer();
