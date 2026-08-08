7d50fafe24190e848e4fba073c1ceea320883d6a
tanvir <mstanvirislam05@gmail.com>
2026-08-08 16:11:34 +0600

    feat: initialize Express backend with Prisma integration
    - Added package.json with dependencies for Express, Prisma, and TypeScript.
    - Created prisma.config.ts for Prisma configuration.
    - Added schema.prisma for database schema definition.
    - Included skills-lock.json for Prisma skills management.
    - Implemented basic Express server in server.ts with CORS and JSON parsing.
    - Added TypeScript configuration in tsconfig.json.
    - Created app.ts as the main application entry point.

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import express from 'express'
+import cors from 'cors'
+
+
+const app = express();
+
+app.use(cors())
+app.use(express.json());
+
+const port = process.env.PORT || 5000
+
+app.listen(port, () => {
+    console.log(`server is running in ${port}`);
+
+})
